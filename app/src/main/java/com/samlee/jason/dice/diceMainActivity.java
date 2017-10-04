4a4facb120e7075f12b985bd83efa2e21ee17d2d
SamLeeXinhua <850731097@qq.com>
2017-10-04 17:45:05 +0800

    Initial Release 1.2 10/4/2017

--- app/src/main/java/com/samlee/jason/dice/diceMainActivity.java
+++ app/src/main/java/com/samlee/jason/dice/diceMainActivity.java
@@ -2,15 +2,24 @@ package com.samlee.jason.dice;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,6 +35,8 @@ public class diceMainActivity extends AppCompatActivity {
     int locator = 0;
     ArrayList recentNum = new ArrayList();
 
+    Handler handler=new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +44,12 @@ public class diceMainActivity extends AppCompatActivity {
 
         //this is the initial dice image, show this first before starting
         final ImageView diceinitImage = (ImageView) findViewById(R.id.diceinit);
+
+        // this is for background , set it as invisable first
+//        final ImageView background = (ImageView) findViewById(R.id.background);
+//        background.setVisibility(View.INVISIBLE);
+        final VideoView firework = (VideoView) findViewById(R.id.firework);
+        firework.setVisibility(View.INVISIBLE);
 
         // this is the bar for point and roll times shown
         final TextView point = (TextView) findViewById(R.id.totalPoint);
@@ -44,13 +61,41 @@ public class diceMainActivity extends AppCompatActivity {
         recentNum.clear();
         //register the dice action
 
+        // create a sound for dice roll
+        final SoundPool diceSoundPool;
+        final SoundPool bonusSoundPool;
+        final SoundPool clearSoundPool;
+
+        // Create a sound pool
+        if (Build.VERSION.SDK_INT <= 15) {
+            //API level 15
+            diceSoundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+            bonusSoundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+            clearSoundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+
+        } else {
+            //API level 21
+            diceSoundPool = new SoundPool.Builder().setMaxStreams(10).build();
+            bonusSoundPool = new SoundPool.Builder().setMaxStreams(10).build();
+            clearSoundPool = new SoundPool.Builder().setMaxStreams(10).build();
+        }
+
+        final int diceSound;
+        final int bonusSound;
+        final int clearSound;
+
+        diceSound = diceSoundPool.load(this , R.raw.coin,1);
+        bonusSound = bonusSoundPool.load(this , R.raw.lync_videocall,1);
+        clearSound = clearSoundPool.load(this , R.raw.windows_notify,1);
+
         diceinitImage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             // catch the touch event here , only down event will be caught.
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()){
                     case MotionEvent.ACTION_DOWN:
-
+                        //play the dice roll music
+                        diceSoundPool.play(diceSound,1,1,0,0,1);
                         // this is used for frame action
                         AnimationDrawable anim = new AnimationDrawable();
                         for (int i = 1; i <= 6 ; i++) {
@@ -66,7 +111,9 @@ public class diceMainActivity extends AppCompatActivity {
                                 e.printStackTrace();
                             }
                         }
-
+//                        Log.i("orginal color:", diceinitImage.ge)
+//                        diceinitImage.setBackgroundColor(0x99000);
+//                        diceinitImage.setImageResource(R.drawable.bk);
                         anim.setOneShot(true);
                         diceinitImage.setImageDrawable(anim);
                         anim.start();
@@ -81,42 +128,79 @@ public class diceMainActivity extends AppCompatActivity {
                         totalNum = totalNum + currentImageNum;
                         recentNum.add(currentImageNum);
 
+
                         // if 3 dices were rolled at continue 3 times, 10 additional bonus will be added
                         if(locator >= 3 && (recentNum.get(locator - 2).equals(recentNum.get(locator - 1)) && recentNum.get(locator - 2).equals(recentNum.get(locator)) )){
                             Toast showCheer = Toast.makeText(getApplicationContext(), "Cheer! , 10 bonus additional ", Toast.LENGTH_LONG);
                             showCheer.show();
                             totalNum = totalNum +10;
-                        }
+                            bonusSoundPool.play(diceSound,1,1,0,0,1);
 
+                            firework.setVisibility(View.VISIBLE);
+
+                            Uri rawUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.fireworks1c);
+                            firework.setVideoURI(rawUri);
+                            firework.start();
+                            firework.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                @Override
+                                public void onCompletion(MediaPlayer mp) {
+                                    Toast Cheer = Toast.makeText(getApplicationContext(), "Cheer! , 10 bonus additional ", Toast.LENGTH_LONG);
+                                    Cheer.show();
+
+                                    firework.setVisibility(View.GONE);
+                                }
+                            });
+                        }
                         locator++;
+//                        Log.i("locator:", ""+locator);
+
                         point.setText("Total bonus : " + totalNum + "\n\nRolled times: " + rollTime++);
                         break;
                     // We can add more action event here
+                    case MotionEvent.ACTION_UP:
+//                        diceinitImage.setBackgroundColor(0);
+                        break;
                 }
+
                 return false;
             }
         });
 
         // clear button
-        clearButton.setOnClickListener(new View.OnClickListener() {
+
+        clearButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                // clear the bonus and roll times
-                totalNum = 0;
-                rollTime = 1;
-                locator = 0;
-                recentNum.clear();
-                try {
-                    // show the initial image
-                    InputStream initStream = getAssets().open("diceinit.png");
-                    Drawable dInit = Drawable.createFromStream(initStream,null);
-                    diceinitImage.setImageDrawable(dInit);
-                } catch (IOException e) {
-                    e.printStackTrace();
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        totalNum = 0;
+                        rollTime = 1;
+                        locator = 0;
+                        recentNum.clear();
+                        try {
+                            // show the initial image
+                            InputStream initStream = getAssets().open("diceinit.png");
+                            Drawable dInit = Drawable.createFromStream(initStream,null);
+                            diceinitImage.setImageDrawable(dInit);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        clearSoundPool.play(clearSound,1,1,0,0,1);
+
+                        point.setText("Total points : 0" + "\n\nRolled times: 0" );
+                        clearButton.setBackgroundColor(0x99000);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        clearButton.setBackgroundResource(R.drawable.dice0);
+                        break;
                 }
-                point.setText("Total points : 0" + "\n\nRolled times: 0" );
+
+                return false;
             }
         });
 
     }
+
 }
+
